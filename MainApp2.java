cc4513ff78369ab32e0cc804d6e3ffef9ef2bb7e
Jalu28 <115241749+Jalu28@users.noreply.github.com>
2022-10-07 20:18:08 +0700

    Delete MainApp2.java

--- MainApp2.java
+++
@@ -1,9 +0,0 @@
-public class MainApp2 {
-    // Ahmad Panjalu R5R 202043501452
-    public static void main(String[] args) {
-        Hewan h = new Hewan();
-
-        h.setEkor("Panjang");
-        System.out.println("Ekornya " + h.getEkor());
-    }
-} 
